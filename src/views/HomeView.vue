3d99cdd4f047ba5f3aa942b4a0e8d3c5c4c59ead
Mustafa Neto <mustafaneto26@gmail.com>
2023-04-25 15:07:55 -0300

    teste/autocomplete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,31 @@
-<script setup>
+<template>
+  <div class="home">
+    <v-autocomplete
+      clearable
+      label="Autocomplete"
+      class="selectCity"
+      :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+      variant="solo"
+    ></v-autocomplete>
+  </div>
+</template>
 
+<script>
+export default {
 
+}
 </script>
 
-<template>
-  <main>
-    
-  </main>
-</template>
+<style scoped>
+  .home{
+    max-width: 1000px;
+    height: max-content;
+    margin: auto;
+    padding: 20px;
+    background-color: white;
+  }
+
+  .selectCity {
+    width: 200px;
+  }
+</style>
